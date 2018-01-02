3881b32ac5fc7b0025c439744f1d44d7dc31221d
sowork <807522127@qq.com>
2018-01-02 09:17:16 +0800

    修改数据库字段默认长度

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -25,6 +26,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        Schema::defaultStringLength(191);
     }
 }
